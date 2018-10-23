bca5608e65a4c28bd3eaa4130f929151de1a1460
Lukáš Tyrychtr <lukastyrychtr@gmail.com>
2018-10-23 19:43:40 +0200

    Add the database access object and its implementation for Genre.

---
+++ src/main/java/cz/muni/fi/pa165/dao/GenreDaoImpl.java
@@ -0,0 +1,30 @@
+package cz.muni.fi.pa165.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import cz.muni.fi.pa165.entity.Genre;
+
+public class GenreDaoImpl implements GenreDao {
+@PersistenceContext
+private EntityManager em;
+	
+public void create(Genre genre) {
+		em.persist(genre);
+	}
+
+	public void remove(Genre genre) {
+		em.remove(genre);
+	}
+
+	public List<Genre> findAll() {
+		return em.createQuery("select g from Genre g").getResultList();
+	}
+
+	public Genre findById(Long id) {
+		return em.find(Genre.class, id);
+	}
+
+}
