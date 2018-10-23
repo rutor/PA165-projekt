1223500e772b761693552a841fe0a9bf25c96a08
Robo <dudasrobert93@gmail.com>
2018-10-23 19:12:00 +0200

    R_User2

--- src/main/java/cz/muni/fi/pa165/entity/Role.java
+++ src/main/java/cz/muni/fi/pa165/entity/Role.java
@@ -1,10 +1,54 @@
 package cz.muni.fi.pa165.entity;
 
 /**
- * Hello world!
+ * Robo
  *
  */
-public class Role
-{
+public class Role{
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToMany
+    @NotNull
+    private List<User> user = new ArrayList<User>();
+
+    @NotNull
+    private String Name;
+
+    private String Description;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<User> getUser() {
+        return user;
+    }
+
+    public void setUser(List<User> user) {
+        this.user = user;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
 }
--- src/main/java/cz/muni/fi/pa165/entity/User.java
+++ src/main/java/cz/muni/fi/pa165/entity/User.java
@@ -1,22 +1,133 @@
 package cz.muni.fi.pa165.entity;
 
 /**
- * Hello world!
+ * Robo
  *
  */
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
 @Entity
-@Table(name="User")
-public class User
-{
+@Table(name="Users")
+public class User {
+
+
     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable=false,unique=true)
-    @Pattern(regexp=".+@.+\\....?")
+    @ManyToOne
+    @NotNull
+    private Role role;
+
+    @OneToMany
+    @NotNull
+    private List<Booking> user = new ArrayList<Booking>();
+
+    @NotNull
+    private String FirstName;
+
+    @NotNull
+    private String Lastname;
+
+    @Column(nullable = false, unique = true)
+    @Pattern(regexp = ".+@.+\\....?")
     @NotNull
     private String email;
 
+    @NotNull
+    private String password;
 
+    @Temporal(TemporalType.DATE)
+    private Date CreatedAt;
+
+    @Temporal(TemporalType.DATE)
+    private Date UpdatedAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public List<Booking> getUser() {
+        return user;
+    }
+
+    public void setUser(List<Booking> user) {
+        this.user = user;
+    }
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String firstName) {
+        FirstName = firstName;
+    }
+
+    public String getLastname() {
+        return Lastname;
+    }
+
+    public void setLastname(String lastname) {
+        Lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getCreatedAt() {
+        return CreatedAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        CreatedAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return UpdatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        UpdatedAt = updatedAt;
+    }
 }
