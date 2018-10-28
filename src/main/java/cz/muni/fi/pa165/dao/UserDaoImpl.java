65ddfc8f992a9c21e484711622713533ba223b20
Robo <dudasrobert93@gmail.com>
2018-10-28 22:44:37 +0100

    final

--- src/main/java/cz/muni/fi/pa165/dao/UserDaoImpl.java
+++ src/main/java/cz/muni/fi/pa165/dao/UserDaoImpl.java
@@ -1,5 +1,6 @@
 package cz.muni.fi.pa165.dao;
 
+import cz.muni.fi.pa165.entity.Role;
 import java.util.List;
 
 import javax.inject.Named;
@@ -22,18 +23,38 @@ public class UserDaoImpl implements UserDao {
     public void remove(User user) {
         em.remove(user);
     }
-
+/*
     public List<User> findAll() {
         return em.createQuery("select g from User g", User.class).getResultList();
     }
 
     public User findById(Long id) {
-        return em.find(User.class, id);
-    }
-
+		return em.find(User.class,  id);
+	}
+*/
     /*public User findByRole(User user ) {
      //   return ;
     }*/
+
+    @Override
+    public void create(Role role) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void remove(Role role) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Role> findAll() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Role findById(Long id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 
 
 
