dc9b219d75ba41dcb904a447a9961e159457b4cc
Robo <dudasrobert93@gmail.com>
2018-10-28 17:19:00 +0100

    UserDaoimpl

--- src/main/java/cz/muni/fi/pa165/dao/UserDaoImpl.java
+++ src/main/java/cz/muni/fi/pa165/dao/UserDaoImpl.java
@@ -1,4 +1,34 @@
 package cz.muni.fi.pa165.dao;
 
-public class UserDaoImpl {
+import java.util.List;
+
+import javax.inject.Named;
+import javax.persistence.*;
+
+import org.springframework.stereotype.Repository;
+
+import cz.muni.fi.pa165.entity.User;
+
+@Repository
+@Named
+public class UserDaoImpl implements UserDao {
+    @PersistenceContext
+    private EntityManager em;
+
+    public void create(User user) {
+        em.persist(user);
+    }
+
+    public void remove(User user) {
+        em.remove(user);
+    }
+
+    public List<User> findAll() {
+        return em.createQuery("select g from Genre g", User.class).getResultList();
+    }
+
+    public User findById(Long id) {
+        return em.find(User.class, id);
+    }
+
 }
