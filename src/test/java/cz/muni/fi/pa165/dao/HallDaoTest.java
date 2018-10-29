99ac0e119caf40a23d56bb1e701caaff63124305
Robo <dudasrobert93@gmail.com>
2018-10-29 13:05:52 +0100

    Implement Tests

---
+++ src/test/java/cz/muni/fi/pa165/dao/HallDaoTest.java
@@ -0,0 +1,61 @@
+package cz.muni.fi.pa165.dao;
+
+
+
+import cz.muni.fi.pa165.ApplicationContext;
+import cz.muni.fi.pa165.entity.*;
+import org.junit.*;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.*;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.inject.Inject;
+import javax.persistence.*;
+
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Robo
+ */
+//public class HallDaoTest{}
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = ApplicationContext.class)
+@TestExecutionListeners(TransactionalTestExecutionListener.class)
+@Transactional
+public class HallDaoTest extends AbstractTestNGSpringContextTests {
+    @PersistenceContext
+    private EntityManager em;
+
+    @Inject
+    private HallDao dao;
+
+    @Test
+    public void createTest() {
+        Hall hall= new Hall();
+        hall.setAddress("Tajovskeho");
+        hall.setCapacity(400L);
+        hall.setName("Velka_sala");
+        
+        em.persist(hall);
+
+        Hall hall1= new Hall();
+        hall1.setAddress("Sladkovicova");
+        hall1.setCapacity(200L);
+        hall1.setName("Mala_sala");
+        
+        em.persist(hall1);
+        
+        dao.create(hall);
+        
+        
+        Hall hallFromDatabase = em.find(Hall.class,hall.getId());
+        assertEquals(hall, hallFromDatabase);
+    }
+
+}
--- src/test/java/cz/muni/fi/pa165/dao/PerformanceDaoTest.java
+++ src/test/java/cz/muni/fi/pa165/dao/PerformanceDaoTest.java
@@ -4,6 +4,7 @@ package cz.muni.fi.pa165.dao;
 
 import cz.muni.fi.pa165.ApplicationContext;
 import cz.muni.fi.pa165.entity.*;
+import java.time.LocalDate;
 import org.junit.*;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.*;
@@ -21,6 +22,8 @@ import static org.junit.Assert.*;
  *
  * @author Robo
  */
+//public class PerformanceDaoTest{}
+
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = ApplicationContext.class)
@@ -31,24 +34,46 @@ public class PerformanceDaoTest extends AbstractTestNGSpringContextTests {
     private EntityManager em;
 
     @Inject
-    private ShowDao dao;
+    private PerformanceDao dao;
 
     @Test
     public void createTest() {
+
+        Hall hall =new Hall();
+        
+        hall.setAddress("Tajovskeho");
+        hall.setCapacity(400L);
+        hall.setName("Velka_sala");
+        
+        em.persist(hall);
+       
         Genre genre = new Genre();
-        genre.setName("Dolor");
-        genre.setDescription("Sit");
-
+        genre.setName("Horror");
+        genre.setDescription("best");
+        
         em.persist(genre);
-
-        Show show = new Show();
-        show.setName("Lorem");
-        show.setDescription("Ipsum");
+           
+        Show show= new Show();
         show.setGenre(genre);
-        dao.create(show);
+        show.setName("The_Strangers_2");
+        show.setDescription("best");
+        show.setGenre(genre);
+        show.setDuration(140);
+        show.setName("The_Strangers_2");
+        
+        em.persist(show);        
+        
+        Performance performance= new Performance();
+        performance.setHall(hall);
+        performance.setPrice(250f);
+        performance.setStartDate(LocalDate.now());
+       
+        //em.persist(performance);
 
-        Show showFromDatabase = em.find(Show.class, show.getId());
-        assertEquals(show, showFromDatabase);
+        dao.create(performance);
+
+        Performance performanceFromDatabase = em.find(Performance.class, performance.getId());
+        assertEquals(performance, performanceFromDatabase);
     }
 
 }
