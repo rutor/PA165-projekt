b57cff8822651521145ebe7b28db4c3ff912d90a
Robo <dudasrobert93@gmail.com>
2018-10-29 11:38:40 +0100

    Implement Tests

--- src/test/java/cz/muni/fi/pa165/dao/PerformanceDaoTest.java
+++ src/test/java/cz/muni/fi/pa165/dao/PerformanceDaoTest.java
@@ -1,9 +1,54 @@
 package cz.muni.fi.pa165.dao;
 
+
+
+import cz.muni.fi.pa165.ApplicationContext;
+import cz.muni.fi.pa165.entity.*;
+import org.junit.*;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.*;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.inject.Inject;
+import javax.persistence.*;
+
+import static org.junit.Assert.*;
+
 /**
  *
  * @author Robo
  */
-public class PerformanceDaoTest {
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = ApplicationContext.class)
+@TestExecutionListeners(TransactionalTestExecutionListener.class)
+@Transactional
+public class PerformanceDaoTest extends AbstractTestNGSpringContextTests {
+    @PersistenceContext
+    private EntityManager em;
+
+    @Inject
+    private ShowDao dao;
+
+    @Test
+    public void createTest() {
+        Genre genre = new Genre();
+        genre.setName("Dolor");
+        genre.setDescription("Sit");
+
+        em.persist(genre);
+
+        Show show = new Show();
+        show.setName("Lorem");
+        show.setDescription("Ipsum");
+        show.setGenre(genre);
+        dao.create(show);
+
+        Show showFromDatabase = em.find(Show.class, show.getId());
+        assertEquals(show, showFromDatabase);
+    }
 
 }
