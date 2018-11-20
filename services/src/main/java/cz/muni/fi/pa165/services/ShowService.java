3831bd28fcd8d9c0a8e67bcb66ca17838998a4fa
Lukáš Tyrychtr <lukastyrychtr@gmail.com>
2018-11-20 20:16:27 +0100

    Adds a crude implementation of the service and facade layers.

--- services/src/main/java/cz/muni/fi/pa165/services/ShowService.java
+++ services/src/main/java/cz/muni/fi/pa165/services/ShowService.java
@@ -8,7 +8,7 @@ public interface ShowService {
 public Long create(Show show);
 public Show findById(Long id);
 public List<Show> findAll();
-public List<Show> findByGenre(Genre genre);
+public List<Show> findAllByGenre(Genre genre);
 public void remove(Show show);
 public void update(Show show);
 }
