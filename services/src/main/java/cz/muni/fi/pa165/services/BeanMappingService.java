8201b1ce449ebe0886cb3193d418fc3e9b703941
Tomáš Rudolf <rutor@rutor.eu>
2018-11-21 21:33:17 +0100

    Fixing conflicts made by 'auto' merge

--- services/src/main/java/cz/muni/fi/pa165/services/BeanMappingService.java
+++ services/src/main/java/cz/muni/fi/pa165/services/BeanMappingService.java
@@ -7,19 +7,9 @@ import org.dozer.Mapper;
 
 
 public interface BeanMappingService {
-<<<<<<< HEAD
-
     public  <T> List<T> mapTo(Collection<?> objects, Class<T> mapToClass);
 
     public  <T> T mapTo(Object u, Class<T> mapToClass);
 
     public Mapper getMapper();
 }
-=======
-	
-    public  <T> List<T> mapTo(Collection<?> objects, Class<T> mapToClass);
-
-    public  <T> T mapTo(Object u, Class<T> mapToClass);
-    public Mapper getMapper();
-}
->>>>>>> milestone2/integration
