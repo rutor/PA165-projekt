80fb4c1f6bf9c9a404709a2aacd9a40ee5400999
xtrnkal <trnkalovaa@seznam.cz>
2018-11-24 15:48:39 +0100

    Added Hall service and implementation

---
+++ services/src/main/java/cz/muni/fi/pa165/services/HallService.java
@@ -0,0 +1,21 @@
+package cz.muni.fi.pa165.services;
+
+import cz.muni.fi.pa165.entity.Hall;
+import java.util.List;
+
+/**
+ *
+ * @author xtrnkal
+ */
+public interface HallService {
+
+    public Long create(Hall hall);
+
+    public Hall findById(Long id);
+
+    public List<Hall> findAll();
+
+    public void remove(Hall hall);
+
+    public void update(Hall hall);
+}
