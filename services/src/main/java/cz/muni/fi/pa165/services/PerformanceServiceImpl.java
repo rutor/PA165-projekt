21a8c768586c16254990e14505663ed374932d96
xtrnkal <trnkalovaa@seznam.cz>
2018-11-24 15:49:09 +0100

    Added Performance service and implementation

---
+++ services/src/main/java/cz/muni/fi/pa165/services/PerformanceServiceImpl.java
@@ -0,0 +1,54 @@
+package cz.muni.fi.pa165.services;
+
+import cz.muni.fi.pa165.dao.PerformanceDao;
+import cz.muni.fi.pa165.entity.Hall;
+import cz.muni.fi.pa165.entity.Performance;
+import cz.muni.fi.pa165.entity.Show;
+import java.util.List;
+import javax.inject.Inject;
+
+/**
+ *
+ * @author xtrnkal
+ */
+public class PerformanceServiceImpl implements PerformanceService {
+
+    @Inject
+    private PerformanceDao performanceDao;
+
+    @Override
+    public Long create(Performance performance) {
+        performanceDao.create(performance);
+        return performance.getId();
+    }
+
+    @Override
+    public Performance findById(Long id) {
+        return performanceDao.findById(id);
+    }
+
+    @Override
+    public List<Performance> findAll() {
+        return performanceDao.findAll();
+    }
+
+    @Override
+    public List<Performance> findAllByHall(Hall hall) {
+        return performanceDao.findAllByHall(hall);
+    }
+
+    @Override
+    public List<Performance> findAllByShow(Show show) {
+        return performanceDao.findAllByShow(show);
+    }
+
+    @Override
+    public void remove(Performance performance) {
+        performanceDao.remove(performance);
+    }
+
+    @Override
+    public void update(Performance performance) {
+        performanceDao.update(performance);
+    }
+}
