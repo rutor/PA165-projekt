eef4cc8a6ee217a9d241b254bd94e63328a9f7a5
Lukáš Tyrychtr <lukastyrychtr@gmail.com>
2018-11-24 14:44:16 +0100

    Some more tests.

--- services/src/test/java/cz/muni/fi/pa165/services/GenreServiceTest.java
+++ services/src/test/java/cz/muni/fi/pa165/services/GenreServiceTest.java
@@ -35,6 +35,8 @@ GenreDao dao;
         @InjectMocks
     private GenreService service;
 
+        private Genre opera;
+        private Genre comedy;
     public Genre createGenre(Long id, String name, String description) {
         Genre g = new Genre();
         g.setId(id);
@@ -46,22 +48,44 @@ GenreDao dao;
     @Before
     public void setup() {
     	MockitoAnnotations.initMocks(this);
+    	opera = createGenre(1l, "Opera", "Však víte.");
+    	comedy = createGenre(2l, "Komedie", "Byla, je a bude");
     }
 
     @Test
 public void testFindById() {
-	Genre testGenre = createGenre(1l, "Opera", "Však víte.");
-	when(dao.findById(1l)).thenReturn(testGenre);
-	assertEquals(service.findById(1l), testGenre);
+	when(dao.findById(1l)).thenReturn(opera);
+	assertEquals(service.findById(1l), opera);
 }
 
     @Test
     public void testFindAll() {
-    	List<Genre> genres = Arrays.asList(createGenre(1l, "Opera", "Dávná"), createGenre(2l, "Komedie", "Byla, je a bude"));
+    	List<Genre> genres = Arrays.asList(opera, comedy);
     	when(dao.findAll()).thenReturn(genres);
     	List<Genre> result = service.findAll();
     	assertEquals(result.size(), 2);
     	assertEquals(genres.get(0), result.get(0));
     	assertEquals(genres.get(1), result.get(1));
     }
+@Test
+public void testCreate() {
+	assertEquals(service.create(opera), new Long(1));
+	verify(dao).create(opera);
+}
+@Test
+public void testRemove() {
+	service.remove(comedy);
+	verify(dao).remove(comedy);
+}
+@Test
+public void testUpdate() {
+	service.update(opera);
+	verify(dao).update(opera);
+}
+
+@Test
+public void testFindByName() {
+	when(dao.findByName("opera")).thenReturn(opera);
+	assertEquals(service.findByName("opera"), opera);
+}
 }
