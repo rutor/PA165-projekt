4a9911bd0678e35d6a7365cb0ae18d127c7af5ed
Lukáš Tyrychtr <lukastyrychtr@gmail.com>
2018-11-26 20:19:38 +0100

    Facade tests.

---
+++ services/src/test/java/cz/muni/fi/pa165/facade/GenreFacadeTest.java
@@ -0,0 +1,128 @@
+package cz.muni.fi.pa165.facade;
+
+import cz.muni.fi.pa165.ApplicationContext;
+import cz.muni.fi.pa165.ServicesContext;
+import cz.muni.fi.pa165.dao.GenreDao;
+import cz.muni.fi.pa165.dto.CreateGenreDTO;
+import cz.muni.fi.pa165.dto.GenreDTO;
+import cz.muni.fi.pa165.entity.Genre;
+import cz.muni.fi.pa165.facade.GenreFacade;
+import cz.muni.fi.pa165.services.GenreService;
+import cz.muni.fi.pa165.services.TestUtils;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import static org.mockito.Mockito.*;
+import org.springframework.test.context.*;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.inject.Inject;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = ServicesContext.class)
+@TestExecutionListeners(TransactionalTestExecutionListener.class)
+@Transactional
+public class GenreFacadeTest extends AbstractTestNGSpringContextTests {
+        @Inject
+    private GenreFacade facade;
+@Inject
+private GenreService service;
+        
+        private CreateGenreDTO newOpera;
+        private CreateGenreDTO newComedy;
+    public CreateGenreDTO getCreateGenreDTO(String name, String description) {
+        CreateGenreDTO cg = new CreateGenreDTO();
+        cg.setName(name);
+        cg.setDescription(description);
+        return cg;
+    }
+
+    @Before
+    public void setup() {
+    	newOpera = getCreateGenreDTO("Opera", "V�ak v�te.");
+    	newComedy = getCreateGenreDTO("Komedie", "Byla, je a bude");
+    }
+    @Test
+    public void testCreateGenre() {
+    	Long id = facade.createGenre(newOpera);
+    	assertNotNull(id);
+    	Genre operaFromDb = service.findById(id);
+assertEquals(newOpera.getName(), operaFromDb.getName());
+assertEquals(newOpera.getDescription(), operaFromDb.getDescription());
+    }
+    
+    @Test
+    public void testGetById() {
+    	Genre test = TestUtils.createGenre("This is",  "a test");
+    	test.setId(null);
+    	Long id = service.create(test);
+    	GenreDTO genreFromDb = facade.getGenreById(id);
+assertDTOAndEntityEquals(genreFromDb, test);
+    }
+@Test
+public void testGetAll() {
+    	Genre test1 = TestUtils.createGenre("This is",  "a test");
+    	test1.setId(null);
+service.create(test1);
+    	Genre test2 = TestUtils.createGenre("This is also", "a test");
+    	test2.setId(null);
+service.create(test2);
+List<GenreDTO> genres = facade.getAllGenres();
+assertEquals(2, genres.size());
+assertDTOAndEntityEquals(genres.get(0), test1);
+assertDTOAndEntityEquals(genres.get(1), test2);
+}
+@Test
+public void testGetByName() {
+Genre test = TestUtils.createGenre("opera", "Dávná");
+test.setId(null);
+service.create(test);
+GenreDTO testFromDb = facade.getGenreByName(test.getName());
+assertDTOAndEntityEquals(testFromDb, test);
+}
+
+@Test
+public void testRemove() {
+    	Genre test1 = TestUtils.createGenre("This is",  "a test");
+    	test1.setId(null);
+service.create(test1);
+    	Genre test2 = TestUtils.createGenre("This is also", "a test");
+    	test2.setId(null);
+service.create(test2);
+facade.removeGenre(test2.getId());
+List<Genre> genres = service.findAll();
+assertEquals(genres.size(), 1);
+assertEquals(test1, genres.get(0));
+}
+
+@Test
+public void testUpdate() {
+    	Genre test1 = TestUtils.createGenre("This is",  "a test");
+    	test1.setId(null);
+service.create(test1);
+GenreDTO newTest1 = new GenreDTO();
+newTest1.setName(test1.getName());
+newTest1.setId(test1.getId());
+newTest1.setDescription("We're describing you differently.");
+facade.updateGenre(newTest1);
+assertDTOAndEntityEquals(newTest1, service.findById(newTest1.getId()));
+}
+private void assertDTOAndEntityEquals(GenreDTO dto, Genre entity) {
+assertEquals(entity.getId(), dto.getId());
+assertEquals(entity.getName(), dto.getName());
+assertEquals(entity.getDescription(), dto.getDescription());
+}
+
+}
---
+++ services/src/test/java/cz/muni/fi/pa165/facade/ShowFacadeTest.java
@@ -0,0 +1,147 @@
+package cz.muni.fi.pa165.facade;
+
+import cz.muni.fi.pa165.ApplicationContext;
+import cz.muni.fi.pa165.ServicesContext;
+import cz.muni.fi.pa165.dao.GenreDao;
+import cz.muni.fi.pa165.dto.CreateGenreDTO;
+import cz.muni.fi.pa165.dto.CreateShowDTO;
+import cz.muni.fi.pa165.dto.GenreDTO;
+import cz.muni.fi.pa165.dto.ShowDTO;
+import cz.muni.fi.pa165.entity.Genre;
+import cz.muni.fi.pa165.entity.Show;
+import cz.muni.fi.pa165.facade.GenreFacade;
+import cz.muni.fi.pa165.services.GenreService;
+import cz.muni.fi.pa165.services.ShowService;
+import cz.muni.fi.pa165.services.TestUtils;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import static org.mockito.Mockito.*;
+import org.springframework.test.context.*;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.inject.Inject;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = ServicesContext.class)
+@TestExecutionListeners(TransactionalTestExecutionListener.class)
+@Transactional
+public class ShowFacadeTest extends AbstractTestNGSpringContextTests {
+        @Inject
+    private ShowFacade facade;
+@Inject
+private GenreService genreService;
+@Inject
+private ShowService showService;
+        
+    
+    @Test
+    public void testCreate() {
+    	Genre g = TestUtils.createGenre("Smuteční píseň", "Byly, jsou, a asi budou");
+		g.setId(null);
+		genreService.create(g);
+		CreateShowDTO s = new CreateShowDTO();
+s.setName("Píseň studenta");
+s.setDescription("Zvláště povedená parodie reality");
+s.setGenreId(g.getId());
+Long id = facade.createShow(s);
+    	assertNotNull(id);
+    	Show showFromDb = showService.findById(id);
+		assertEquals(s.getName(), showFromDb.getName());
+	}
+    @Test
+    public void testGetById() {
+    	Show test = insertShow("Testy a já", "Co se stalo a nestalo", 412, "Komedie", "Víte, to se sejdou...");
+    	ShowDTO testFromDb = facade.getShowById(test.getId());
+assertDTOAndEntityEquals(testFromDb, test);
+    }
+@Test
+public void testGetAll() {
+    	Show test1 = insertShow("O škole a lidech", "Pravdivá, i když děsivá upomínka", 123, "Horor", "Po pĹůlnoci nedívat");
+    	Show test2 = insertShow("A i oni byli", "Nic nezatajujeme", 24, "Horory a strašidelné", "Po půlnoci nedívat");
+List<ShowDTO> shows = facade.getAllShows();
+assertEquals(2, shows.size());
+assertDTOAndEntityEquals(shows.get(0), test1);
+assertDTOAndEntityEquals(shows.get(1), test2);
+}
+@Test
+public void testGetAllByGenre() {
+Show test = insertShow("O škole a lidech", "Pravdivá, i když děsivá upřímná", 123, "Horor", "Po půlnoci nedívat");
+Show test2 = TestUtils.createShow("Ve větru",  "I tam se dějí věci",  TestUtils.createGenre("Komedie",  "Je, byla, bude"),  123);
+test2.setId(null);;
+test2.getGenre().setId(test.getGenre().getId());
+showService.create(test2);
+insertShow("Pro poslední naději", "Zbyly jen vzpomínky", 221, "Horor a katastrofické", "Radši odejděte");
+List<ShowDTO> matching = facade.getAllShowsByGenreId(test.getGenre().getId());
+assertEquals(2, matching.size());
+assertDTOAndEntityEquals(matching.get(0), test);
+assertDTOAndEntityEquals(matching.get(1), test2);
+}
+
+@Test
+public void testRemove() {
+	Show test1 = insertShow("O škole a lidech", "Pravdivá, i když děsivá upomínka", 123, "Horor a další předpůlnoční", "Po pĹůlnoci nedívat");
+	Show test2 = insertShow("A i oni byli", "Nic nezatajujeme", 24, "Horor", "Po půlnoci nedívat");
+	facade.removeShow(test2.getId());
+List<Show> shows = showService.findAll();
+assertEquals(shows.size(), 1);
+assertEquals(test1, shows.get(0));
+}
+
+@Test
+public void testUpdate() {
+    	 Show test = insertShow("Co stalo se před půlnocí", "Příběh ne úplně vydařeného školního projektu", 144, "Tragikomedie", "I tak to někdy dopadá");
+		 GenreDTO g = new GenreDTO();
+		 ShowDTO s = new ShowDTO();
+g.setName(test.getGenre().getName());
+g.setId(test.getGenre().getId());
+g.setDescription(test.getGenre().getDescription());
+s.setName(test.getName());
+s.setId(test.getId());
+s.setDescription("Zvláště povedená parodie reality");
+s.setGenre(g);
+facade.updateShow(s);
+assertDTOAndEntityEquals(s, showService.findById(s.getId()));
+}
+
+private void assertDTOAndEntityEquals(GenreDTO dto, Genre entity) {
+assertEquals(entity.getId(), dto.getId());
+assertEquals(entity.getName(), dto.getName());
+assertEquals(entity.getDescription(), dto.getDescription());
+}
+
+
+private void assertDTOAndEntityEquals(ShowDTO dto, Show entity) {
+assertEquals(entity.getId(), dto.getId());
+assertEquals(entity.getName(), dto.getName());
+assertEquals(entity.getDescription(), dto.getDescription());
+assertEquals(dto.getDuration(), entity.getDuration());
+assertDTOAndEntityEquals(dto.getGenre(), entity.getGenre());
+}
+
+private Show insertShow(String name, String description, int duration, String genreName, String genreDescription) {
+Genre g = new Genre();
+g.setName(genreName);
+g.setDescription(genreDescription);
+genreService.create(g);
+Show s = new Show();
+s.setName(name);
+s.setDescription(description);
+s.setDuration(duration);
+s.setGenre(g);
+showService.create(s);
+return s;
+}
+}
