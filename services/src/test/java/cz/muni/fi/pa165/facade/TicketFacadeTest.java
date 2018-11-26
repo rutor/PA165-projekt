87ff486cac51165bc8d7abb097815ac6de2d65fb
Tomáš Rudolf <rutor@rutor.eu>
2018-11-27 00:04:35 +0100

    Ticket facade test

---
+++ services/src/test/java/cz/muni/fi/pa165/facade/TicketFacadeTest.java
@@ -0,0 +1,61 @@
+package cz.muni.fi.pa165.facade;
+
+import cz.muni.fi.pa165.ServicesContext;
+import cz.muni.fi.pa165.dto.TicketDTO;
+import cz.muni.fi.pa165.entity.*;
+import cz.muni.fi.pa165.services.BeanMappingService;
+import cz.muni.fi.pa165.services.TicketService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.inject.Inject;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = ServicesContext.class)
+@TestExecutionListeners(TransactionalTestExecutionListener.class)
+@Transactional
+public class TicketFacadeTest extends AbstractTestNGSpringContextTests {
+
+    @Mock
+    private TicketService ticketService;
+
+    @Mock
+    private BeanMappingService beanMappingService;
+
+    @Inject
+    //@InjectMocks
+    private TicketFacade ticketFacade;
+
+    private Ticket ticket1;
+    private Ticket ticket2;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        ticket1 = new Ticket();
+        ticket2 = new Ticket();
+    }
+
+    @Test
+    public void getAllTest() {
+        when(ticketService.getAll()).thenReturn(Arrays.asList(ticket1, ticket2));
+        assertEquals(2, ticketService.getAll().size());
+        List<TicketDTO> ticketDTOs = ticketFacade.getAll();
+        assertEquals(2, ticketDTOs.size());
+    }
+}
