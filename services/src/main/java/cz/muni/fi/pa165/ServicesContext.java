820f648addd127d4b1b2eee340abba0432900d05
Tomáš Rudolf <rutor@rutor.eu>
2018-12-10 23:18:43 +0100

    Added java8 types to dozer

--- services/src/main/java/cz/muni/fi/pa165/ServicesContext.java
+++ services/src/main/java/cz/muni/fi/pa165/ServicesContext.java
@@ -7,12 +7,20 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Configuration
 @ComponentScan(basePackages = {"cz.muni.fi.pa165.services", "cz.muni.fi.pa165.facade"})
 @Import(ApplicationContext.class)
 public class ServicesContext {
 	@Bean
 	public Mapper mapper() {
-		return new DozerBeanMapper();
+		List<String> mappingFiles = new ArrayList();
+		mappingFiles.add("dozerJdk8Converters.xml");
+		//mappingFiles.add("UUIDMapping.xml");
+		DozerBeanMapper dozerBeanMapper = new DozerBeanMapper();
+		dozerBeanMapper.setMappingFiles(mappingFiles);
+		return dozerBeanMapper;
 	}
 }
