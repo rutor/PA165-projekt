3c4a1fbdde2d2425a9daab0157bb8827cc2be9aa
xtrnkal <trnkalovaa@seznam.cz>
2018-12-15 14:32:35 +0100

    edited administration controller

--- web/src/main/java/cz/muni/fi/pa165/mvc/controllers/AdministrationController.java
+++ web/src/main/java/cz/muni/fi/pa165/mvc/controllers/AdministrationController.java
@@ -1,20 +1,8 @@
 package cz.muni.fi.pa165.mvc.controllers;
 
-import cz.muni.fi.pa165.facade.GenreFacade;
-import cz.muni.fi.pa165.facade.ShowFacade;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
-import org.springframework.validation.ObjectError;
-import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import org.springframework.web.util.UriComponentsBuilder;
-
-import javax.validation.Valid;
-import javax.inject.Inject;
-import java.util.List;
 
 /**
  *
@@ -22,22 +10,16 @@ import java.util.List;
  */
 @Controller
 @RequestMapping(value = "/administration")
-public class AdministrationController {
-
-    	@Inject
-	private GenreFacade genreFacade;
-	@Inject
-	private ShowFacade showFacade;
+public class AdministrationController {;
     
     /**
-	 * Shows a list of genres, for admins with the ability to add, delete or edit.
+	 * Shows administration section
 	 * 
 	 * @param model data to display
 	 * @return JSP page name
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String list(Model model) {
-		model.addAttribute("genres", genreFacade.getAllGenres());
 		return "administration";
 	}
     
