adb000c0511159b6aa3fea8b00e611cdde504813
Tomáš Rudolf <rutor@rutor.eu>
2018-12-17 23:18:23 +0100

    Moved into config folder

--- web/src/main/java/cz/muni/fi/pa165/mvc/config/SecurityWebApplicationInitializer.java
+++ web/src/main/java/cz/muni/fi/pa165/mvc/config/SecurityWebApplicationInitializer.java
@@ -2,6 +2,5 @@ package cz.muni.fi.pa165.mvc.controllers;
 
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 
-public class SecurityWebApplicationInitializer
-        extends AbstractSecurityWebApplicationInitializer{
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer{
 }
